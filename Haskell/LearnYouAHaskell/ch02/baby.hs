f5b341913492485ddbed19f165263c952fc99949
Paul Barry <mail@paulbarry.com>
2009-03-20 23:31:25 -0400

    Added Chapter 2 of 'Learn You a Haskell'

---
+++ Haskell/LearnYouAHaskell/ch02/baby.hs
@@ -0,0 +1,9 @@
+doubleMe x = x + x
+doubleUs x y = doubleMe x + doubleMe y
+doubleSmallNumber x = if x > 100
+                        then x
+                        else x*2
+doubleSmallNumber' x = (if x > 100 then x else x*2) + 1                          
+conanO'Brien = "It's a-me, Conan O'Brien!"
+
+boomBangs xs = [ if x < 10 then "BOOM!" else "BANG!" | x <- xs, odd x]
