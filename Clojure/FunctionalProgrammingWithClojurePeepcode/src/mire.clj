08f9d00767fcc13e3769c8af28ff48e62751b6c7
Paul Barry <mail@paulbarry.com>
2009-04-26 13:44:51 -0400

    Added commands to mire

--- Clojure/FunctionalProgrammingWithClojurePeepcode/src/mire.clj
+++ Clojure/FunctionalProgrammingWithClojurePeepcode/src/mire.clj
@@ -1,11 +1,19 @@
 (ns mire
+  (:use [mire commands])
   (:use [clojure.contrib server-socket duck-streams]))
+
+(def port 3333)
+(def prompt "> ")
   
-(defn mire-handle-client [in out]
+(defn- mire-handle-client [in out]
   (binding [*in* (reader in)
             *out* (writer out)]
-          (loop []
-            (println (read-line))
-            (recur))))
+    (print prompt)
+    (flush)
+    (loop [input (read-line)]
+      (println (execute input))
+      (print prompt)
+      (flush)
+      (recur (read-line)))))
             
-(def server (create-server 3333 mire-handle-client))
+(def server (create-server port mire-handle-client))
---
+++ Clojure/FunctionalProgrammingWithClojurePeepcode/src/mire/commands.clj
@@ -0,0 +1,17 @@
+(ns mire.commands
+  (:use [clojure.contrib str-utils]))
+  
+(defn current-time []
+  (str "It is now " (java.util.Date.)))
+  
+(def commands {
+  "time" current-time
+  "look" (fn [] "You see an empty room, waiting to be filled")})
+  
+(defn execute
+  "Execute a command that is passed to us."
+  [input]
+  (let [input-words (re-split #"\s+" input)
+        command (first input-words)
+        args (rest input-words)]
+        (apply (commands command) args)))    
