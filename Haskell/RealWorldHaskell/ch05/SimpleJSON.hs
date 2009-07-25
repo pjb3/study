6903ff268bf4ca32fdd7490335dd2da80446a81a
Paul Barry <mail@paulbarry.com>
2009-07-24 22:51:27 -0400

    Added scheme, real world haskell

---
+++ Haskell/RealWorldHaskell/ch05/SimpleJSON.hs
@@ -0,0 +1,11 @@
+data JValue = JString String
+            | JNumber Double
+            | JBool Bool
+            | JNull
+            | JObject [(String, JValue)]
+            | JArray [JValue]
+              deriving (Eq, Ord, Show)
+              
+getString :: JValue -> Maybe String
+getString (JString s) = Just s
+getString _           = Nothing              
