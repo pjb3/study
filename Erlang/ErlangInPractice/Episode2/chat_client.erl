a37c588dac4ec46b79dd4d1f09ef33f577e03fcc
Paul Barry <mail@paulbarry.com>
2009-08-21 00:50:30 -0400

    Episode 2 of Erlang In Practice

---
+++ Erlang/ErlangInPractice/Episode2/chat_client.erl
@@ -0,0 +1,18 @@
+-module(chat_client).
+
+-compile(export_all).
+
+register_nickname(Nickname) ->
+  message_router:register_nick(Nickname, fun(Msg) -> chat_client:print_message(Nickname, Msg) end).
+
+unregister_nickname(Nickname) ->
+  message_router:unregister_nick(Nickname).
+
+send_message(Addressee, MessageBody) ->
+  message_router:send_chat_message(Addressee, MessageBody).
+  
+print_message(Who, MessageBody) ->
+  io:format("~p received: ~p~n", [Who, MessageBody]).
+  
+start_router() ->
+  message_router:start().
---
+++ Erlang/ErlangInPractice/Episode2/message_router.erl
@@ -0,0 +1,45 @@
+-module(message_router).
+
+-define(SERVER, ?MODULE).
+
+-compile(export_all).
+
+start() ->
+  Pid = spawn(?MODULE, route_messages, [dict:new()]),
+  erlang:register(?SERVER, Pid).
+
+stop() ->
+  ?SERVER ! shutdown.
+
+send_chat_message(Addressee, MessageBody) ->
+  ?SERVER ! {send_chat_msg, Addressee, MessageBody}.
+
+register_nick(ClientName, PrintFun) ->
+  ?SERVER ! {register_nick, ClientName, PrintFun}.
+
+unregister_nick(ClientName) ->
+  ?SERVER ! {unregister_nick, ClientName}.
+
+route_messages(Clients) ->
+  receive
+    {send_chat_msg, ClientName, MessageBody} ->
+      ?SERVER ! {recv_chat_msg, ClientName, MessageBody},
+      route_messages(Clients);
+    {recv_chat_msg, ClientName, MessageBody} ->
+      case dict:find(ClientName, Clients) of
+        {ok, PrintFun} ->
+          PrintFun(MessageBody);
+        error ->
+          io:format("Unknow client~n")
+      end,
+      route_messages(Clients);
+    {register_nick, ClientName, PrintFun} ->
+      route_messages(dict:store(ClientName, PrintFun, Clients));
+    {unregister_nick, ClientName} ->
+      route_messages(dict:erase(ClientName, Clients));
+    shutdown ->
+      io:format("Shutting down~n");
+    Oops ->
+      io:format("Warning! Received: ~p~n", [Oops]),
+      route_messages(Clients)
+  end.
